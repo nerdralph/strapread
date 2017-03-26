a4652c3c143248ce1fe8611f6ef45f6b11d57061
NerdRalph <ralphdoncaster@gmail.com>
2017-03-26 10:17:34 -0300

    zero 32AW

--- strapmod.py
+++ strapmod.py
@@ -4,11 +4,19 @@
 import sys
 
 strap = sys.argv[1]
+
 print "Old, new RRD:", strap[28], ", 5"
 strap = strap[:28] + '5' + strap[29:]
+
 print "Old, new ACTRD:", strap[80:82], ", 0x10"
 strap = strap[:80] + '10' + strap[82:]
+
 print "Old, new FAW:", strap[51], ", 0"
 strap = strap[:51] + '0' + strap[52:]
+
+t32AW = int(strap[52],16)
+print "Old, new 32AW:", t32AW/2, ", 0"
+strap = strap[:52] + '0' + strap[53:]
+
 print sys.argv[1]
 print strap
