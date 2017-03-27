1f77258d12885677b44259a2c28722fe6bc0719c
NerdRalph <ralphdoncaster@gmail.com>
2017-03-26 22:50:42 -0300

    fix SEQ_MISC_TIMING

--- gendef.py
+++ gendef.py
@@ -13,7 +13,7 @@ def genunion(reg):
 
 genunion(['tRCDW:5', 'tRCDWA:5', 'tRCDR:5', 'tRCDRA:5', 'tRRD:4', 'tRC:7', 'SEQ_RAS_TIMING'])
 genunion(['tNOPW:2', 'tNOPR:2', 'tR2W:5', 'tCCDL:3', 'tR2R:4', 'tW2R:8', 'tCL:8', 'SEQ_CAS_TIMING'])
-genunion(['tRP_WRA:8', 'tRP_RDA:7', 'tRP:5', 'tRFC:9', 'SEQ_MISC_TIMING'])
+genunion(['tRP_WRA:7', 'tRP_RDA:7', 'tRP:6', 'tRFC:9', 'SEQ_MISC_TIMING'])
 genunion(['PA2RDATA:4', 'PA2WDATA:4', 'FAW:5', 'tREDC:3', 'tWEDC:5', 't32AW:7', 'tWDATATR:4', 'SEQ_MISC_TIMING2'])
 genunion(['tCKSRE:4', 'tCKSRX:4', 'tCKE_PULSE:4', 'tCKE:6', 'SEQ_IDLE:5', 'tCKE_PULSE_MSB:1', 'SEQ_IDLE_SS:8', 'SEQ_PMG_TIMING'])
 genunion(['RAS2RAS:8', 'RP:8', 'WRPLUSRP:8', 'BUS_TURN:8', 'ARB_DRAM_TIMING2'])
